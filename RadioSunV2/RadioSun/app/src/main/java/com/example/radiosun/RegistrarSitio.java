0b41e86fbcd72deccc80c157aed6efd9b415955e
Jorgealza9206 <jaalzar@correo.udistrital.edu.co>
2021-11-22 20:58:21 -0500

    bbb

--- RadioSunV2/RadioSun/app/src/main/java/com/example/radiosun/RegistrarSitio.java
+++ RadioSunV2/RadioSun/app/src/main/java/com/example/radiosun/RegistrarSitio.java
@@ -1,8 +1,8 @@
 package com.example.radiosun;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class RegistrarSitio extends AppCompatActivity {
 
