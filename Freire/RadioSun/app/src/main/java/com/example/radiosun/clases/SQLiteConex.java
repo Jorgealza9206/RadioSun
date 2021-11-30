0b3e6bf49c9bfc40f81e0b80714613c31af1f991
Freire Palomino <freirealexander0214@gmail.com>
2021-11-30 10:04:28 -0500

    Subiendo Carpeta con archivos de proyecto

---
+++ Freire/RadioSun/app/src/main/java/com/example/radiosun/clases/SQLiteConex.java
@@ -0,0 +1,42 @@
+package com.example.radiosun.clases;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class SQLiteConex extends SQLiteOpenHelper
+{
+
+    private Context context;
+
+    public static final  String nombredb = "Radiosun.db";
+
+    public  SQLiteConex(@Nullable Context c){
+        super(c, nombredb,null,1);
+        this.context = c;
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE sitios"+
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "nombre TEXT NOT NULL," +
+                "radiacion REAL NOT NULL," +
+                "consumo BLOB NOT NULL," +
+                "mes_1 REAL NOT NULL," +
+                "mes_2 REAL NOT NULL," +
+                "mes_3 REAL NOT NULL," +
+                "mes_4 REAL NOT NULL," +
+                "mes_5 REAL NOT NULL," +
+                "mes_6 REAL NOT NULL," +
+                "potencia_del_panel REAL NOT NULL," +
+                "numero_de_paneles REAL NOT NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE sitios");
+        onCreate(db);
+    }
+}
